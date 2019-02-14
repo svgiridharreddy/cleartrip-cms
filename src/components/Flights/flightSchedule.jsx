5ee7eb1468c1b36d733c78d5a2a55017ef41d881
naveenslm <naveen@codingmart.com>
2019-02-14 11:06:13 +0530

    RTE editor implemented with heading tags

--- src/components/Flights/flightSchedule.jsx
+++ src/components/Flights/flightSchedule.jsx
@@ -106,6 +106,26 @@ class FlightScheduleFields extends Component {
       cityNameSelected
 
     } = this.props;
+
+    const toolbarConfig = {
+    // Optionally specify the groups to display (displayed in the order listed).
+    display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
+    INLINE_STYLE_BUTTONS: [
+      {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
+      {label: 'Italic', style: 'ITALIC'},
+      {label: 'Underline', style: 'UNDERLINE'}
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      {label: 'Normal', style: 'unstyled'},
+      {label: 'H1', style: 'header-one'},
+      {label: 'H2', style: 'header-two'},
+      {label: 'H3', style: 'header-three'}
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      {label: 'UL', style: 'unordered-list-item'},
+      {label: 'OL', style: 'ordered-list-item'}
+    ]
+  };
     subTypeField = (
       <Form.Group as={Col}>
         <Form.Label>Sub PageType</Form.Label>
@@ -219,6 +239,7 @@ class FlightScheduleFields extends Component {
          <RichTextEditor
         value={this.state.content}
         onChange={this.onChange1}
+        toolbarConfig={toolbarConfig}
         />
          {/*<Editor
             editorState={this.state.editorState}
