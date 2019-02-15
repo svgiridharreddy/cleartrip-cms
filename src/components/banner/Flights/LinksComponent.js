8a3df1436a71d5dd3a985da9e4029d82bfba34b2
soreddysirish <ssireeshreddy@gmail.com>
2019-02-15 09:55:00 +0530

    added header links functionality

--- src/components/banner/Flights/LinksComponent.js
+++ src/components/banner/Flights/LinksComponent.js
@@ -13,11 +13,19 @@ class LinksComponent extends Component {
 							anchorName: "about bangalore",
 							anchorUrl: "about hyderabad"
 						}
+					],
+					how_to_reach: [
+						{
+							anchorName: "reach",
+							anchorUrl: "reah url"
+						}
 					]
+
 				}
 			],
 			RsideBarLinks: [],
-			FooterLinks: []
+			FooterLinks: [],
+			showErrormsg: false
 		};
 		this.addTag = this.addTag.bind(this);
 		this.headerAddBtn = this.headerAddBtn.bind(this);
@@ -28,14 +36,22 @@ class LinksComponent extends Component {
 			let ele = node.querySelector(".tag_name");
 			let headerTagsList = this.state.headerLinks;
 			if (
-				!headerTagsList.includes(ele.value) ||
-				headerTagsList.length == 0
+				(!headerTagsList.includes(ele.value) ||
+					headerTagsList.length == 0) &&
+				ele.value != ""
 			) {
 				headerTagsList.push({
 					[ele.value]: [{ anchorName: "", anchorUrl: "" }]
 				});
+				this.setState({
+					showErrormsg: false
+				});
+				ele.value = "";
+			} else {
+				this.setState({
+					showErrormsg: true
+				});
 			}
-			ele.value = "";
 			this.setState({
 				headerLinks: headerTagsList
 			});
@@ -44,27 +60,40 @@ class LinksComponent extends Component {
 	handleChange(field, tag, e) {
 		let _self = this;
 		let tag_name = tag;
-		console.log(tag_name);
 		let target = parseInt(e.target.dataset.tag.split("-")[1]);
 		let headerData = _self.state.headerLinks;
 		headerData = headerData.map((obj, k) => {
-			Object.keys(obj).map((sub, j) => {
-				if (tag_name == sub && target == j) {
-					obj[sub].map((asc, k) => {
-						asc[field] = e.target.value;
-					});
-				}
-			});
+			if (obj[tag_name]) {
+				obj[tag_name].map((sub, b) => {
+					if (b == target) {
+						sub[field] = e.target.value;
+					}
+				});
+			}
 			this.setState({ headerLinks: headerData });
 		});
 	}
-	headerAddBtn(menuName,linkName,e) {
-		let _self = this
-		let linkData = _self.state[linkName]
-		debugger
-		linkData = linkData[menuName].map((a,b) => {
-			debugger
-		})
+	headerAddBtn(menuName, linkName, e) {
+		let _self = this;
+		let presentall = true;
+		let linkData = _self.state[linkName];
+		linkData = linkData.map((link, k) => {
+			if (link[menuName]) {
+				link[menuName].map((sub, j) => {
+					if (sub.anchorUrl === "" || sub.anchorName === "") {
+						presentall = false;
+						_self.setState({ showErrormsg: true });
+					}
+				});
+				if (presentall) {
+					link[menuName].push({ anchorName: "", anchorUrl: "" });
+					_self.setState({ showErrormsg: false });
+				}
+			}
+			_self.setState({
+				_self
+			});
+		});
 	}
 
 	renderForm = data => {
@@ -72,7 +101,7 @@ class LinksComponent extends Component {
 			return data[sub].map((asc, k, arr) => {
 				return (
 					<li keys={k} key={k}>
-						<b>{k == 0 ? sub : ""}: </b>
+						<b>{k == 0 ? sub + ":" : ""} </b>
 						<input
 							type="text"
 							name="ancorTagName"
@@ -96,22 +125,30 @@ class LinksComponent extends Component {
 							)}
 						/>
 						{k == arr.length - 1 ? (
-								<button
-									type="button"
-									className="plusButton"
-									data-btnid={k}
-									data-btnname="add"
-									onClick={this.headerAddBtn.bind(this, sub,"headerLinks")}
-								>
-									+
-								</button>
+							<button
+								type="button"
+								className="plusButton"
+								data-btnid={k}
+								data-btnname="add"
+								onClick={this.headerAddBtn.bind(
+									this,
+									sub,
+									"headerLinks"
+								)}
+							>
+								+
+							</button>
 						) : (
 							<button
 								type="button"
 								className="plusButton"
 								data-btnid={k}
 								data-btnname="sub"
-								onClick={this.headerAddBtn.bind(this, sub,"headerLinks")}
+								onClick={this.headerAddBtn.bind(
+									this,
+									sub,
+									"headerLinks"
+								)}
 							>
 								-
 							</button>
@@ -126,6 +163,11 @@ class LinksComponent extends Component {
 		let _self = this;
 		return (
 			<div>
+				{_self.state.showErrormsg ? (
+					<p className="errormsg">*please enter all fields</p>
+				) : (
+					""
+				)}
 				<h2>Header Links</h2>
 				<ul className="list-inline">
 					<li>
