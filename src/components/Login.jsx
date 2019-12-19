48c690e8fd7c20f8bc7985e4a321b36b13a30fcd
soreddysirish <ssireeshreddy@gmail.com>
2019-12-19 10:06:11 +0530

    login post call

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -56,104 +56,104 @@ class Login extends Component {
     if (
       loginCredentials["username"] != "" &&
       loginCredentials["password"] != ""
-    ) {
+      ) {
       _self.setState({
         showErrormsg: false,
         loginCredentials
       });
-      return true;
-    } else {
-      _self.setState({
-        showErrormsg: true
-      });
-      return false;
-    }
-  }
-  checkCredentials(e) {
-    e.preventDefault()
-    let _self = this;
-    let loginCredentials = _self.state.loginCredentials;
-    if (this.handleValidate()) {
-      axios
-        .get(this.state.host + "/user/login", {
-          params: loginCredentials
-        })
-        .then(resp => {
-          let data = resp.data["authenticated"];
-          this.props.changeState()
-          localStorage.setItem("user_data", JSON.stringify(data));
-          _self.setState({
-            showErrormsg: false,
-            show: false,
-            loginStatus: true
-          });
-          NotificationManager.success("successfully login", "login", 1500);
-          setTimeout(function () {
-            window.location.reload()
-          }, 1800)
-        })
-        .catch(err => {
-          loginCredentials["password"] = "";
-          _self.setState({
-            showErrormsg: true,
-            loginCredentials: loginCredentials
-          });
-        });
-    }
-  }
-  resetForm() {
-    let _self = this
-    let loginCredentials = _self.state.loginCredentials
-    loginCredentials["username"] = "";
-    loginCredentials["password"] = "";
+    return true;
+  } else {
     _self.setState({
-      loginCredentials
+      showErrormsg: true
+    });
+    return false;
+  }
+}
+checkCredentials(e) {
+  e.preventDefault()
+  let _self = this;
+  let loginCredentials = _self.state.loginCredentials;
+  if (this.handleValidate()) {
+    axios({
+      url:this.state.host + "/user/login",
+      method:'post',
+      data: loginCredentials
+
+    }).then(resp => {
+      let data = resp.data
+      this.props.changeState()
+      localStorage.setItem("user_data", JSON.stringify(data));
+      _self.setState({
+        showErrormsg: false,
+        show: false,
+        loginStatus: true
+      });
+      NotificationManager.success("successfully login", "login", 1500);
+      setTimeout(function () {
+        window.location.reload()
+      }, 1800)
+    }).catch(err => {
+      loginCredentials["password"] = "";
+      _self.setState({
+        showErrormsg: true,
+        loginCredentials: loginCredentials
+      });
     })
   }
-  render() {
-    let _self = this;
-    let loginCredentials = _self.state.loginCredentials;
-    return (
-      <div>
-        <Button variant="primary" onClick={this.handleShow.bind(this)}>
-          Login
-        </Button>
-        <Modal show={this.state.show} onHide={this.handleClose.bind(this)} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>Login to CMS</Modal.Title>
-          </Modal.Header>
-          <span className={_self.state.showErrormsg ? "errormsg" : "hidden"}>
-            plese enter correct credentials
-          </span>
-          <Modal.Body>
-            <form onSubmit={this.checkCredentials.bind(this)}>
-              <Form.Group role="form">
-                <Form.Label>Enter username</Form.Label>
-                <Form.Control placeholder="Enter username/email" type="text"
-                  name="username"
-                  value={loginCredentials.username}
-                  onChange={this.handleChange.bind(this, "username")} />
-              </Form.Group>
-              <Form.Group role="form">
-                <Form.Label>Enter Password</Form.Label>
-                <Form.Control placeholder="password"
-                  type="password"
-                  name="password"
-                  value={loginCredentials.password}
-                  onChange={this.handleChange.bind(this, "password")} />
-              </Form.Group>
-              <ButtonToolbar>
-                <Button type="submit"  onClick={this.checkCredentials.bind(this)} className="btn  btn-large  loginSubmitbtn">
-                  Login
-            </Button>
-                <Button variant="secondary" onClick={this.resetForm.bind(this)} className ="loginResetbtn">Reset</Button>
-              </ButtonToolbar>
-            </form>
-          </Modal.Body>
-        </Modal>
-        <NotificationContainer />
-      </div>
+}
+resetForm() {
+  let _self = this
+  let loginCredentials = _self.state.loginCredentials
+  loginCredentials["username"] = "";
+  loginCredentials["password"] = "";
+  _self.setState({
+    loginCredentials
+  })
+}
+render() {
+  let _self = this;
+  let loginCredentials = _self.state.loginCredentials;
+  return (
+    <div>
+    <Button variant="primary" onClick={this.handleShow.bind(this)}>
+    Login
+    </Button>
+    <Modal show={this.state.show} onHide={this.handleClose.bind(this)} centered>
+    <Modal.Header closeButton>
+    <Modal.Title>Login to CMS</Modal.Title>
+    </Modal.Header>
+    <span className={_self.state.showErrormsg ? "errormsg" : "hidden"}>
+    plese enter correct credentials
+    </span>
+    <Modal.Body>
+    <form onSubmit={this.checkCredentials.bind(this)}>
+    <Form.Group role="form">
+    <Form.Label>Enter username</Form.Label>
+    <Form.Control placeholder="Enter username/email" type="text"
+    name="username"
+    value={loginCredentials.username}
+    onChange={this.handleChange.bind(this, "username")} />
+    </Form.Group>
+    <Form.Group role="form">
+    <Form.Label>Enter Password</Form.Label>
+    <Form.Control placeholder="password"
+    type="password"
+    name="password"
+    value={loginCredentials.password}
+    onChange={this.handleChange.bind(this, "password")} />
+    </Form.Group>
+    <ButtonToolbar>
+    <Button type="submit"  onClick={this.checkCredentials.bind(this)} className="btn  btn-large  loginSubmitbtn">
+    Login
+    </Button>
+    <Button variant="secondary" onClick={this.resetForm.bind(this)} className ="loginResetbtn">Reset</Button>
+    </ButtonToolbar>
+    </form>
+    </Modal.Body>
+    </Modal>
+    <NotificationContainer />
+    </div>
     );
-  }
+}
 }
 export default Login;
